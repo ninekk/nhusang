4b5d7485aee12e61130ae78583661acdc975aabe
iamnii <iamnii@LAPTOP-PLHTE556.lan>
2024-04-06 10:48:21 +0700

    Hoc OOP

---
+++ src/com/nii/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.nii;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello world! Niiii");
+		 
+		 SinhVien sinhVien = new SinhVien();
+		 sinhVien.niii = "vdvsdv";
+		 sinhVien.lien = "fff";
+		 
+		 sinhVien.setId(11);
+		 
+//		 sout
+		 
+		 System.out.println(sinhVien.lien);
+		 System.out.println(sinhVien.getId());
+
+	}
+
+}
---
+++ src/com/nii/SinhVien.java
@@ -0,0 +1,34 @@
+package com.nii;
+
+public class SinhVien {
+	private int id;
+	private String name;
+	private String address;
+	public String niii;
+	protected String lien;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+	
+}
